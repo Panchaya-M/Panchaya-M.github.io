30dd2189c71afacee48363591694e28992266b4d
Panchaya-M <Panchaya.meeboonpor@gmail.com>
2023-10-29 17:18:55 +0700

    display title relate to fillter category

--- products.js
+++ products.js
@@ -3,6 +3,7 @@ import { products, creatCardProduct } from "./script.js";
 const urlParams = new URLSearchParams(window.location.search);
 const categoryFilter = urlParams.get("category");
 
+displayTitle();
 if (!categoryFilter) {
   createProducts(products);
 } else {
@@ -14,5 +15,26 @@ function createProducts(products) {
   let element = document.getElementById('product-container');
   for(let product of products) {
     creatCardProduct(element, product);
+  }
+}
+
+function displayTitle() {
+  let titleElement = document.getElementById('title-products');
+  let title = getTitle();
+  titleElement.textContent = title;
+}
+
+function getTitle() {
+  switch (categoryFilter) {
+    case 'vase':
+      return 'VASEA';
+    case 'candle':
+      return 'CANDLES';
+    case 'tray':
+      return 'TRAYS';
+    case 'lamp':
+      return 'LAMPS'
+    default:
+      return 'ALL PRODUCTS';
   }
 }
